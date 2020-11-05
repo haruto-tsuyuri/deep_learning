bff727df4040445464648f0e25139b3052f12441
TSUYURI <tsuyuri@grit-group.co.jp>
2020-11-05 11:30:05 +0900

    特殊メソッドファイル
    https://docs.python.org/ja/3/reference/datamodel.html

---
+++ PythonCh6/word.py
@@ -0,0 +1,19 @@
+class Word:
+    def __init__(self, text):
+        self.text = text
+
+    def __eq__(self, word2):
+        return self.text.lower() == word2.text.lower()
+
+    def __str__(self):
+        return self.text
+
+    def __repr__(self):
+        return 'Word("' + self.text + '")'
+
+
+first = Word('hey')
+second = Word('hey there')
+
+print(first)
+
